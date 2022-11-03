bcee9fa0f306aaa7350edc4bfdad397fab5aa3db
Marko Blagus <marko.blagus@smartis.hr>
2022-11-03 15:41:40 +0100

    evo jos nes novo

--- bzvz.java
+++ bzvz.java
@@ -1,5 +1,6 @@
 public class bzvz {
     public static void main(String[] args) {
         System.out.println("ova klasa ce ispisivati sto god joj se kaze");
+        System.out.println("Nadodali smo nesto novo");
     }
 }
