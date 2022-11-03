7fd4ac7ff8b620310d6449afeb452801b01161a5
Marko Blagus <marko.blagus@smartis.hr>
2022-11-03 15:49:03 +0100

    poruka

--- Super.java
+++ Super.java
@@ -1,5 +1,7 @@
 public class Super {
     public static void main(String[] args) {
         System.out.println("Ovo ce se ispisivati u mainu");
+        System.out.println("zasto ne dodati jos nekoeliko linija");
+        System.out.println("mozda jos jednu");
     }
 }
